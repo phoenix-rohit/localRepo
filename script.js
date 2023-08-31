52d462e0afa7ad34b9f21f614cac46d050981ea4
phoenix rohit <rohitrane88888@gmail.com>
2023-08-31 18:16:40 +0530

    Add a new iterator

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 'use strict';
 const jsIsFun = true;
+
+const country = ['Russia', 'Mongolia', 'China', 'Nepal', 'India', 'Srilanka']
+
+country.forEach(function (ele, ind, array) {
+    console.log(`${ind + 1} : ${ele} of total ${array}`);
+})
